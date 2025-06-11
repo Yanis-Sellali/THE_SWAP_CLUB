bb10dd6dc8bf8e753050b1b84a159bfb38e8382b
Bruno Lopes lopesbruno90@gmail.com <lopesbruno90@gmail.com>
2025-06-11 19:32:05 +0200

    bull

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show]
 
   def show
-    @user = User.find_by(id: params[:id]) # `find_by` évite une erreur fatale si l'utilisateur n'existe pas.
+    @user = User.find_by(id: params[:id]) 
   if@user.nil?
-    redirect_to root_path, alert: "Utilisateur non trouvé." # Redirige si aucun utilisateur n'est trouvé.
+    redirect_to root_path, alert: "Utilisateur non trouvé."
   else
     @jerseys = @user.jerseys
     @exchanges = @user.exchanges
