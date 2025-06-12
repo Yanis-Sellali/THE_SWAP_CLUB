8cbf303306c5f4463d34b6f12a141bee4150e130
toshirompika <toshirompika@gmail.com>
2025-06-12 17:53:53 +0200

    modifier le profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,25 @@ class UsersController < ApplicationController
       @exchanges = @user.exchanges
     end
   end
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Profil mis à jour avec succès."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def index
     @users = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nom, :localisation)
   end
 end
