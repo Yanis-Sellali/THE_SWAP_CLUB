bbd9be5ebead2f364261748a02859ec3f60ff3b2
Dylane Piro <piro.dylan@outlook.fr>
2025-06-17 12:04:14 +0200

    turbo stream for chat + messages/fixes on controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_16_154919) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_17_095447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,12 +89,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_16_154919) do
     t.string "message_type"
     t.integer "offer_jersey_user1_id"
     t.integer "offer_jersey_user2_id"
-    t.boolean "finalized_by_user1"
-    t.boolean "finalized_by_user2"
     t.boolean "user1_finalized"
     t.boolean "user2_finalized"
     t.index ["chat_id"], name: "index_messages_on_chat_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.text "channel"
+    t.text "payload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 
   create_table "teams", force: :cascade do |t|
